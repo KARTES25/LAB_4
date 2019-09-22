c446d4ca8308cd73ff7d52277cb94f950a1b46ef
KARTES <stilalex@yandex.ru>
2019-09-22 22:57:05 +0700

    for Dany

--- lab-4.js
+++ lab-4.js
@@ -1,31 +1,24 @@
-let images = new Array();
+let images = ['./s1.gif','./s2.gif','./s3.gif','./s4.gif','./s5.gif','./s6.gif','./s7.gif','./s8.gif','./s9.gif','./s10.gif','./s11.gif','./s12.gif'];
 let i = 0;
 let j = 1000;
- 
-images[0] = './s1.gif';
-images[1] = './s2.gif';
-images[2] = './s3.gif';
-images[3] = './s4.gif';
-images[4] = './s5.gif';
-images[5] = './s6.gif';
-images[6] = './s7.gif';
-images[7] = './s8.gif';
-images[8] = './s9.gif';
-images[9] = './s10.gif';
-images[10] = './s11.gif';
-images[11] = './s12.gif';
- 
-    function viewImages() {
-    document.getElementById('img_main').src = images[i]; 
-    i++;
-    if (i == images.length) {
-        i = 0;
+    function bis(){
+        j=j-300;
     }
-    let timerId = setTimeout("viewImages()",j);
+    function med(){
+        j=j+300;
+    }
+    function stop(){
+        j = Infinity;
+    }
+    function viewImages() {
+        document.getElementById('img_main').src = images[i]; 
+        i++;
+        if (i == images.length) {
+            i = 0;
+        }
+    setTimeout("viewImages()",j);
     };
     
-    function bis(){j=j-300;}
-    function med(){j=j+300;}
-    function stop(){clearTimeout(timerId);}
+    
    
 
